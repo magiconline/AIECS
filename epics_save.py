fa99215a1ac2ef33cd258695b9a1e540e9b99d33
魔法张 <597482326@qq.com>
2021-11-22 18:19:23 +0800

    add epics_save

---
+++ epics_save.py
@@ -0,0 +1,21 @@
+import epics
+import csv
+import sys
+import time
+
+def save():
+    pass
+
+def save_by_time():
+    pass
+
+def save_by_change():
+    pass
+
+def save_all():
+    pass
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    print(sys.platform)
